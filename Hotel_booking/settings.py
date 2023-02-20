0d59da783aa17cea2d02c0401346ddaef20e0dce
Harsh Godkar <harshgodkar14@gmail.com>
2023-02-20 12:09:24 +0530

    Some changes

--- Hotel_booking/settings.py
+++ Hotel_booking/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'Hotel_booking.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- Registration/models.py
+++ Registration/models.py
@@ -8,3 +8,47 @@ class Registration(models.Model):
     def __str__(self):
         return self.username
 
+class Customer(models.Model):
+    cust_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=10)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Admin(models.Model):
+    name = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=10)
+    email_id = models.EmailField()
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Payment(models.Model):
+    payment_id = models.IntegerField()
+    cus_id = models.IntegerField()
+    payment_type = models.CharField(max_length=50)
+    payment_amt = models.IntegerField()
+    payment_date = models.DateField()
+
+class Booking(models.Model):
+    booking_id = models.IntegerField()
+    booking_desc = models.TextField()
+    booking_date = models.DateField()
+
+class Room(models.Model):
+    room_id = models.IntegerField()
+    room_no = models.CharField(max_length=10)
+    room_type = models.CharField(max_length=30)
+    room_desc = models.TextField()
+
+class Receptionist(models.Model):
+    res_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.TextField()
+    dob = models.DateField()
